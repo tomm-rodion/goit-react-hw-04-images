252df778efbeeed69c34275f78569b10b6ab7db4
tomm-rodion <autovolna@gmail.com>
2023-02-13 16:28:56 +0200

    corection previous refactoring code

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,17 +1,30 @@
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 
 export const Modal = ({ largeImg, onClose }) => {
   const modalRef = document.querySelector('#root-modal');
-  window.addEventListener('keydown', () => onCloseByEsc);
+
+  useEffect(() => {
+    window.addEventListener('keydown', onCloseByEsc);
+    return () => {
+      window.removeEventListener('keydown', onCloseByEsc);
+    };
+  });
+
+  const handleBackdrop = e => {
+    if (e.currentTarget === e.target) {
+      onClose();
+    }
+  };
 
   const onCloseByEsc = e => {
     if (e.code === 'Escape') {
-      return onClose();
+      onClose();
     }
   };
 
   return createPortal(
-    <div className="Overlay" onClick={onClose}>
+    <div className="Overlay" onClick={handleBackdrop}>
       <div className="Modal">
         <img src={largeImg} alt="bigImg" />
       </div>
